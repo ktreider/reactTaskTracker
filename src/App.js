5932b81cf4a2e2c3439138969222bec8d2383962
Katie Reider <ktreider5@gmail.com>
2022-01-29 21:45:52 -0500

    Start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg'; //for the logo to import
+//import './App.css'; //for the basic css styling to import
 
+//this is the root app component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    //looks like HTML but this is that JSX 
+    //className is the new 'class' 
+    <div className="container">
+      <h1>Hello from React</h1>
+
+      <h2>Hiya</h2>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+//entry point for react is here
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+//app is the root component! so <App/> on line 12 is grabbing the App component
+//which is imported on line 5
+
+ReactDOM.render( //grabs root div and insert the react app into that div
   <React.StrictMode>
-    <App />
+    <App />  
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
